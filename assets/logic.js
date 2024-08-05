d4619478fe12d8a2f77d0ac8246bc0ff08d65d88
vmarorti <marsmotionless98@gmail.com>
2024-08-05 08:32:07 -0500

    Organized files and folders/added css

--- assets/logic.js
+++
@@ -1,9 +0,0 @@
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
-    const options = {
-      method: 'GET',
-      headers: {
-          'x-rapidapi-key': '25b9d2b2c38027aae95ac20ad2c8fc20',
-          'x-rapidapi-host': 'https://home.openweathermap.org/api_keys'
-      }
-    };
